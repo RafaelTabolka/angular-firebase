3753c9b0615279edff5b27f83799f53d3fabdec3
RafaelTabolka <rafaeltabolka@hotmail.com>
2022-07-18 11:42:44 -0300

    adicionados firebase

--- app/auth/auth-routing.module.ts
+++ app/auth/auth-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodosComponent } from './pages/todos/todos.component';
 
-const routes: Routes = [
-  {
-    path:'',
-    component:TodosComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TodosRoutingModule { }
+export class AuthRoutingModule { }
